713a3b47ab6a7f4d631af9273412d74dbdece9ae
Yota321 <golamsayan@gmail.com>
2025-04-20 20:50:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold mb-4">Spotlight</h1>
+      <p className="text-lg">Your mood-based music explorer</p>
+    </div>
+  );
+}
+
+export default App;
